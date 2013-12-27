0c9ebc3a3fd754adc981dc2b37c31b1fe83ff159
reenboog <alex.gievsky@gmail.com>
2013-12-27 20:06:23 +0700

    Added some hats.

---
+++ Classes/CountTo.cpp
@@ -0,0 +1,9 @@
+//
+//  CountTo.cpp
+//  old28
+//
+//  Created by reenboog on 24.12.13.
+//
+//
+
+#include "CountTo.h"
---
+++ Classes/CountTo.h
@@ -0,0 +1,14 @@
+//
+//  CountTo.h
+//  old28
+//
+//  Created by reenboog on 24.12.13.
+//
+//
+
+#ifndef __old28__CountTo__
+#define __old28__CountTo__
+
+#include <iostream>
+
+#endif /* defined(__old28__CountTo__) */
